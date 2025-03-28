0992d28f04a5e628582a4c6d1d989e753b00f996
nitesh <rs.nitesh01@gmail.com>
2025-03-29 02:14:42 +0530

    GET API endpoints to fetch student details and List has been created...

--- app.js
+++ app.js
@@ -15,3 +15,38 @@ connectToDb((err) => {
         db = getDb();
     }
 })
+
+app.get('/api/students', (req, res) => {
+    const page = parseInt(req.query.p) || 0;
+    const studentsPerPage = 10;
+    let students = [];
+    db.collection('students').find()
+        .sort({ id: 1 })
+        .skip(studentsPerPage * page)
+        .limit(studentsPerPage)
+        .forEach(student => {
+            students.push(student);
+        })
+        .then(() => {
+            res.status(200).json(students);
+        })
+        .catch(() => {
+            res.status(500).json({msg: 'Something went wrong.'});
+        })
+});
+
+app.get('/api/students/:id', (req, res) => {
+    const studentId = parseInt(req.params.id);
+    if(!isNaN(studentId)){
+        db.collection('students')
+            .findOne({ id: studentId })
+            .then((student) => {
+                res.status(200).json(student);
+            })
+            .catch(() => {
+                res.status(500).json({msg: 'Something went wrong.'});
+            })
+    } else {
+        res.status(404).json({msg: 'Not a valid id'})
+    }
+})
